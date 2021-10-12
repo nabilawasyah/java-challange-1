af424bba6c6a14a30790fc416b8db58abc125cbe
nabilawasyah <nwasyah@gmail.com>
2021-10-12 13:27:58 +0700

    first commit

---
+++ ChallangeTugas.java
@@ -0,0 +1,18 @@
+public class ChallangeTugas {
+    public static void main (String[] args){
+    String firstName = "Nabila ";
+    String lastName = "Wasyah ";
+    int age = 21;
+    String address = "Batam ";
+    String phoneNumber = "085834257148 ";
+    String hobi = "Menari ";
+    String makes = "Seblak, Mie ayam ";
+    System.out.println("first name : " + firstName);
+    System.out.println("last name : " + lastName);
+    System.out.println("Age : " + age);
+    System.out.println("address : " + address);
+    System.out.println("phoneNumber : " + phoneNumber);
+    System.out.println("hobi : " + hobi);
+    System.out.println("makanan kesukaan : " + makes);
+    }    
+}
